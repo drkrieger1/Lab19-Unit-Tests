42bf1afff8583a51126db579000604a22bfc4051
drkrieger <eplyushko@gmail.com>
2017-10-08 20:15:37 -0700

    model, controllers, views added

---
+++ Lab19erik/Lab19erik/Model/FakeLocationRepo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab16Erik.Model
+{
+    public class FakeLocationRepo : ILocationRepo
+    {
+        public IEnumerable<Location> Location => new List<Location>
+        {
+            new Location {Name = "Hawaii", Latitude = 19.896766, Longitede = -155.582782, WeatherType = "Tropical"},
+            new Location {Name ="Alaska", Latitude = 61.174235, Longitede = -149.982012, WeatherType = " Arctic"},
+            new Location {Name = "Texas", Latitude = 31.968599, Longitede = -99.901813, WeatherType = "Maritime"},
+            new Location {Name = "Florida", Latitude = 27.664827, Longitede = -81.515754, WeatherType = "Tropical"}
+        };
+    }
+}
---
+++ Lab19erik/Lab19erik/Model/ILocationRepo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab16Erik.Model
+{
+    public interface ILocationRepo
+    {
+        IEnumerable<Location> Location { get; }
+    }
+}
