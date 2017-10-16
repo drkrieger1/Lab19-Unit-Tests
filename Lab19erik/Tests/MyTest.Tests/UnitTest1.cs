90a190628f7a7e17ea994f0fb406c90a900b8e62
drkrieger <eplyushko@gmail.com>
2017-10-15 22:05:13 -0700

    tests added

--- Lab19erik/Tests/MyTest.Tests/UnitTest1.cs
+++ Lab19erik/Tests/MyTest.Tests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using Lab19erik.Model;
 using System;
 using Xunit;
 
@@ -7,7 +8,28 @@ namespace MyTest.Tests
     {
         [Fact]
         public void Test1()
-        {
+        {   
+            //Arrange
+            var l = new Location { Name="seattle" };
+
+            //Act 
+            l.Name = "Kent";
+
+            //Assert
+            Assert.Equal("Kent", l.Name);
+        }
+
+        [Fact]
+        public void Test2()
+        {   
+            //Arrange
+            var l = new Location();
+
+            //Act 
+            l.Name = "Renton";
+
+            //Assert
+            Assert.NotNull(l); 
 
         }
     }
